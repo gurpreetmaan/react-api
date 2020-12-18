a576460cb85065afb2a3513dee9169f61727f9ec
ShineD-153(Satya) <ShineD-153@domain.wgs.local>
2020-12-18 15:21:44 +0530

    first api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import './ghibliapi.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	fetch('https://ghibliapi.herokuapp.com/films')
+		.then((response) => {
+			console.log(response, "response")
+			return response.json()
+		})
+		.then((data) => {
+			// Work with JSON data here
+			// console.log('data', data)
+			const movies = data.map(movie => {
+				console.log('movie', movie)
+				// return "hello";
+				return (
+					<div className="card">
+						<h1>{movie.title}</h1>
+						<p>{movie.description.substr(0, 300)}...</p>
+					</div>
+				)
+			})
+
+			console.log('list', movies)
+
+			return movies;
+		})
+		.catch((err) => {
+			console.log("error");
+			// Do something for an error here
+		})
+	// const movieList="hsd";
+	return (
+		<div className="main-content">
+			<img src="logo.png" alt="logo" />
+			<div className="container"></div>
+		</div>
+	);
 }
 
 export default App;
